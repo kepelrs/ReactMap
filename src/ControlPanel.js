9f00cd0ecc68423af7aa10b1a6e32674014d8684
Davi Barreto <davivbarreto@gmail.com>
2018-07-29 14:49:22 +0300

    Removing all create-react-app starter code

---
+++ src/ControlPanel.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ControlPanel(props) {
+  return (
+    <div>I'm a the control panel</div>
+  );
+}
+
+export default ControlPanel;
---
+++ src/MapContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function MapContainer(props) {
+  return (
+    <div>I'm a the MapContainer</div>
+  );
+}
+
+export default MapContainer;
