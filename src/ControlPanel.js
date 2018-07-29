6a116dfcb6eb17f36a84ca7c14e352b0827b7cf3
Davi Barreto <davivbarreto@gmail.com>
2018-07-29 20:00:04 +0300

    Add toggle mobile menu functionality

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -3,15 +3,19 @@ import React from 'react';
 class ControlPanel extends React.Component {
   state = {
     query: '',
-    panelOpen: true
+    mobileDisplayingList: false
+  }
+
+  toggleDisplayList = () => {
+    this.setState(state=>({mobileDisplayingList: !state.mobileDisplayingList}))
   }
 
   render (){
     return (
     <div className="control-panel">
       <div className="filter-locations">Im the input</div>
-      <div className="toggle-locations-list">Im the open menu toggle</div>
-      <div className="locations-list open">Im the locations</div>
+      <div className="toggle-locations-list" onClick={this.toggleDisplayList}>Im the open menu toggle</div>
+      <div className={"locations-list" + (this.state.mobileDisplayingList ? " open" : "")}>Im the locations</div>
     </div>
     );
   }
