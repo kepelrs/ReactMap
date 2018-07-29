a099663691590df14d39ae314ea1c6d727c2193a
Davi Barreto <davivbarreto@gmail.com>
2018-07-29 19:17:52 +0300

    Make ControlPanel responsive with CSS

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-function ControlPanel(props) {
-  return (
-    <div className="control-panel">I'm a the control panel</div>
-  );
+class ControlPanel extends React.Component {
+  state = {
+    query: '',
+    panelOpen: true
+  }
+
+  render (){
+    return (
+    <div className="control-panel">
+      <div className="filter-locations">Im the input</div>
+      <div className="toggle-locations-list">Im the open menu toggle</div>
+      <div className="locations-list open">Im the locations</div>
+    </div>
+    );
+  }
 }
 
 export default ControlPanel;
