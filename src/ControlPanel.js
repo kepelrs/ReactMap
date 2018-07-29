c9848af9fca88d423fe2e985504d556463b7d932
Davi Barreto <davivbarreto@gmail.com>
2018-07-29 21:26:09 +0300

    Add input field

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -10,10 +10,22 @@ class ControlPanel extends React.Component {
     this.setState(state=>({mobileDisplayingList: !state.mobileDisplayingList}))
   }
 
+  updateQuery = (query) => {
+    this.setState({query})
+  }
+
   render (){
     return (
     <div className="control-panel">
-      <div className="filter-locations">Im the input</div>
+      <div className="filter-locations">
+        <input
+          className='search-contacts'
+          type='text'
+          placeholder='&#xF002; Search'
+          value={this.state.query}
+          onChange={(event) => this.updateQuery(event.target.value)}
+        />
+      </div>
       <div className="toggle-locations-list" onClick={this.toggleDisplayList}>Im the open menu toggle</div>
       <div className={"locations-list" + (this.state.mobileDisplayingList ? " open" : "")}>Im the locations</div>
     </div>
