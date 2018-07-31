085df9c21103f968da2c61f416ddd137b2afd208
Davi Barreto <davivbarreto@gmail.com>
2018-07-31 15:32:45 +0300

    Fetch latLng from location string on app start

--- src/App.js
+++ src/App.js
@@ -5,11 +5,38 @@ import MapContainer from './MapContainer';
 
 class App extends Component {
   state = {
-    favoriteCities: [
-      {name: 'Fortaleza'},
-      {name: 'Campinas'},
-      {name: 'Sozopol'}
-    ]
+    startingPoints: [
+      'Fortaleza',
+      'Campinas',
+      'Sozopol'
+    ],
+    favoriteCities: []
+  }
+
+  componentDidMount(){
+    const favoriteCities = [];
+    const promises = this.state.startingPoints.map(point=>{
+      return this.fetchGeocoding(point).then(result=>favoriteCities.push(result))
+    })
+    Promise.all(promises).then(()=>this.setState({favoriteCities}))
+  }
+
+  fetchGeocoding = (queryString)=>{
+    return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${queryString}&key=AIzaSyDCr3LVMPH6XG5mopjT0DHmL8uTLQ-2OrI`)
+    .then(response=>response.json())
+    .then(MapsAPIResponse=>{
+      if(MapsAPIResponse && MapsAPIResponse.status === "OK") {
+        const firstMatch = MapsAPIResponse.results[0]
+        return {
+          name: queryString,
+          fullName: firstMatch.formatted_address,
+          coordinates: firstMatch.geometry.location,
+        }
+      } else {
+        return {}
+      }
+    })
+    .catch(()=>({}))
   }
 
   render() {
