5caff2c1be45984b02feb6cb4e7df3814290e942
Davi Barreto <davivbarreto@gmail.com>
2018-07-31 17:31:11 +0300

    Create fetchNews function

--- src/App.js
+++ src/App.js
@@ -39,6 +39,11 @@ class App extends Component {
     .catch(()=>({}))
   }
 
+  fetchNews = (city)=>{
+    return fetch(`https://newsapi.org/v2/everything?q=${city}&language=en&pageSize=100&sortBy=relevance&apiKey=ef7ff90d5a7f4729b4ee1ba38c112156`)
+    .then(response=>response.json())
+  }
+
   render() {
     return (
       <div className="App">
