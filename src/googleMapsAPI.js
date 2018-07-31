8b56a45b6dc02699ad0b64904c44dff01185740c
Davi Barreto <davivbarreto@gmail.com>
2018-07-31 20:08:49 +0300

    Refactor: Better store/query full city names

--- src/googleMapsAPI.js
+++ src/googleMapsAPI.js
@@ -2,16 +2,20 @@ export const fetchGeocoding = (queryString) => {
   return fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${queryString}&key=AIzaSyDCr3LVMPH6XG5mopjT0DHmL8uTLQ-2OrI`)
   .then(response=>response.json())
   .then(MapsAPIResponse=>{
+    const cityObject = {}
+
     if(MapsAPIResponse && MapsAPIResponse.status === "OK") {
-      const firstMatch = MapsAPIResponse.results[0]
-      return {
-        name: queryString,
-        fullName: firstMatch.formatted_address,
-        coordinates: firstMatch.geometry.location,
-      }
-    } else {
-      return {}
+      const bestMatch = MapsAPIResponse.results[0]
+      const cityName = bestMatch.address_components[0].long_name
+      const countryName = bestMatch.address_components[1].long_name
+      const location = bestMatch.geometry.location
+
+      cityObject.name = queryString
+      cityObject.fullName = cityName + ", " + countryName
+      cityObject.coordinates = location
     }
+
+    return cityObject
   })
   .catch(()=>({}))
 }
